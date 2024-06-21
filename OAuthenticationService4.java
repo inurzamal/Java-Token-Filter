e267e382e07e7e55bbcfb111832a4681b306eb09
Nurzamal Islam <n.zaman98@gmail.com>
2024-06-21 11:26:31 +0530

    Update OAuthenticationService4.java

--- OAuthenticationService4.java
+++ OAuthenticationService4.java
@@ -1,133 +1,56 @@
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.reactive.function.client.WebClientResponseException;
 
 import javax.annotation.PostConstruct;
-import java.util.Date;
-import java.util.Properties;
+import java.nio.charset.StandardCharsets;
+import java.time.Instant;
+import java.util.Base64;
+import java.util.Map;
+import java.util.concurrent.locks.ReentrantLock;
+import reactor.core.publisher.Mono;
 
-@Service
-public class EaiPricingInterface {
+@Component
+public class OAuthenticationService {
 
-    private static final String RATE_XPATH = "//EAIPricingResponse/Body/BaseRateResultSet/BaseRate [WhichBaseRateEntry='Final']/BaseRate";
-    private static final String STATUSCODE_XPATH = "//EAIPricingResponse/Body/ReturnCode";
-    private static final String STATUSMSG_XPATH = "//EAIPricingResponse/Body/ErrorMessage";
-    private static final String XMLHDR = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>";
-    private static final String EXCLUSION_MSG_XPATH = "//EAIPricingResponse/Body/ExclusionResultSet/Exclusion";
+    @Value("${OATH_AUTHORIZATION}")
+    private String authString;
 
-    private XMLUtil xmlUtil;
-    private static final Logger log = LoggerFactory.getLogger(EaiPricingInterface.class);
+    @Value("${OATH_SERVICE_END_POINT_URL}")
+    private String authUrl;
 
-    @Autowired
-    private ProcessControlHandler control;
-
-    @Autowired
-    private OAuthenticationService oAuthenticationService;
-
-    @Autowired
-    private SoapServiceInvoker soapServiceInvoker;
+    private WebClient webClient;
+    private String accessToken;
+    private Instant expiryTime;
+    private final ReentrantLock lock = new ReentrantLock();
 
     @PostConstruct
     private void init() {
-        this.xmlUtil = XMLUtil.getInstance();
+        this.webClient = WebClient.builder().baseUrl(authUrl).build();
     }
 
-    public EaiPricingResponse getRate(EaiPricingRequest_Celws request) {
-        StringBuffer sb = xmlUtil.startXmlDocument(XMLHDR);
-        xmlUtil.openElement(sb, "EAIPricingRequest");
-
-        // Construct the XML request
-        xmlUtil.openElement(sb, "Header");
-        xmlUtil.addElement(sb, "CorrelationId", request.getAccountNumber());
-        xmlUtil.addElement(sb, "requestorID", request.getRequestorId());
-        xmlUtil.addElement(sb, "CreatorId", "CLEAHEQ");
-        xmlUtil.addElement(sb, "AuthorizationId", "NONE");
-        xmlUtil.addElement(sb, "ApplicationCode", "BT");
-        xmlUtil.addElement(sb, "ChannelCode", "INT");
-        xmlUtil.addElement(sb, "AccountingUnit");
-        xmlUtil.addElement(sb, "CreationTimestamp", new Date(), "yyyyMMddHHmmssSSS");
-        xmlUtil.addElement(sb, "MessageType", "RQST");
-        xmlUtil.addElement(sb, "Timeout", 30000);
-        xmlUtil.closeElement(sb, "Header");
-
-        // Start Body of Document
-        xmlUtil.openElement(sb, "Body");
-        xmlUtil.addElement(sb, "AmortizationTerm", request.getTerm());
-        xmlUtil.addElement(sb, "AmortizationType", request.getAmortizationType());
-        xmlUtil.addElement(sb, "AutoPay", request.isAutopay());
-        xmlUtil.addElement(sb, "Employee", request.isEmployee());
-        xmlUtil.addElement(sb, "Premier", request.isPremier());
-        xmlUtil.addElement(sb, "CLTV", request.getCltv());
-        xmlUtil.addElement(sb, "CreditGrade", request.getCreditGrade());
-        xmlUtil.addElement(sb, "Fico1", request.getFico1());
-        xmlUtil.addElement(sb, "Fico2", request.getFico2());
-        xmlUtil.addElement(sb, "FicoScore", request.getFicoScore());
-        xmlUtil.addElement(sb, "LienPosition", request.getLienPosition());
-
-        if (request.getNoteAmount() >= 0.01f && request.getNoteAmount() < 1.0f) {
-            xmlUtil.addElement(sb, "LoanAmount", 1);
-        } else {
-            xmlUtil.addElement(sb, "LoanAmount", StringUtil.toInt(request.getNoteAmount()));
-        }
-
-        xmlUtil.addElement(sb, "LoanClass", request.getLoanClass());
-        xmlUtil.addElement(sb, "Occupancy", request.getOccupancy());
-        xmlUtil.addElement(sb, "PrePayYears", request.getPrePayYears());
-        xmlUtil.addElement(sb, "Product", request.getProduct());
-        xmlUtil.addElement(sb, "ProductFeature", request.getProductFeature());
-        xmlUtil.addElement(sb, "PropertyType", request.getPropertyType());
-        xmlUtil.addElement(sb, "QuoteDate", request.getQuoteDate(), "yyyyMMddHHmmssSSS");
-        xmlUtil.addElement(sb, "PropertyState", request.getPropertyState());
-        xmlUtil.addElement(sb, "PricingState", request.getPricingState());
-        xmlUtil.addElement(sb, "ChannelSource", request.getChannelSource());
-        xmlUtil.addElement(sb, "MarketSource", request.getMarketSource());
-        xmlUtil.addElement(sb, "MiscMarginAdjuster", "");
-        xmlUtil.addElement(sb, "MiscRateAdjuster", request.getRateAdjusters());
-        xmlUtil.addElement(sb, "CallerId", "42");
-        xmlUtil.closeElement(sb, "Body");
-        xmlUtil.closeElement(sb, "EAIPricingRequest");
-
-        if (request.isCaptureXml()) {
-            request.setXml(sb.toString());
-        }
-        log.info("CELWS Pricing Request: {}", sb);
-
-        String eaiURL = StringUtils.trimToNull(control.getControlValue(ProcessControlConstants.EAI_URL));
-        if (eaiURL != null) {
-            log.debug("R2.19:: EaiPricingInterface: URL in getRate: EaiPricingRequest_Celws:: {}", eaiURL);
-            try {
-                String result = soapServiceInvoker.invokeService(eaiURL, sb.toString(), new Properties() {{
-                    put(CleaConstants.CONTENT_TYPE, "text/xml");
-                    put("Authorization", getBearerToken());
-                }});
-
-                result = StringUtil.removeString(result, "xmlns=\"http://HEQAPPAZPHX01.wellsfargo.com/\"");
-
-                Document doc = xmlUtil.getXMLDocument(result);
-
-                log.info("EaiPricingInterface: CELWS Pricing Response: {}", doc.asXML());
-
-                EaiPricingResponse response = new EaiPricingResponse();
-                response.setRate(xmlUtil.getNodeValue(doc, RATE_XPATH));
-                response.setStatusCode(xmlUtil.getNodeValue(doc, STATUSCODE_XPATH));
-                response.setStatusMsg(xmlUtil.getNodeValue(doc, STATUSMSG_XPATH));
-                response.setExclusionMsg(xmlUtil.getNodeValue(doc, EXCLUSION_MSG_XPATH));
-
-                if (request.isCaptureXml() && response != null) {
-                    response.setXml(result);
-                }
-
-                return response;
-            } catch (Exception e) {
-                log.error("Error processing EAI pricing request", e);
-                throw new RuntimeException("Error processing EAI pricing request", e);
+    public Mono<String> getOathAccessToken() {
+        lock.lock();
+        try {
+            if (accessToken == null || Instant.now().isAfter(expiryTime)) {
+                return webClient.post()
+                        .header("Authorization", "Basic " + Base64.getEncoder().encodeToString(authString.getBytes(StandardCharsets.UTF_8)))
+                        .header("Content-Type", "application/x-www-form-urlencoded")
+                        .retrieve()
+                        .bodyToMono(new ParameterizedTypeReference<Map<String, String>>() {})
+                        .doOnNext(response -> {
+                            accessToken = response.get("access_token");
+                            int expiresIn = Integer.parseInt(response.get("expires_in"));
+                            expiryTime = Instant.now().plusSeconds(expiresIn - 60);  // Subtract 60 seconds to be safe
+                        })
+                        .map(response -> accessToken)
+                        .onErrorMap(WebClientResponseException.class, ex -> new RuntimeException("Error fetching token", ex));
+            } else {
+                return Mono.just(accessToken);
             }
-        } else {
-            log.error("EAI URL not configured.");
-            throw new IllegalStateException("EAI URL not configured.");
+        } finally {
+            lock.unlock();
         }
-    }
-
-    private String getBearerToken() {
-        return "Bearer " + oAuthenticationService.getOathAccessToken().block();
     }
 }
